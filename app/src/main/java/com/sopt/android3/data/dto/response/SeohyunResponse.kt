7f4ee68289161b620ab4da657012c2febc6cf45a
Hiimynameiss <sh2410152@sookmyung.ac.kr>
2026-05-17 04:08:59 +0900

    [feat] #5 uiState 제작 및 전용 컴포넌트 제작

---
+++ app/src/main/java/com/sopt/android3/data/dto/response/SeohyunResponse.kt
@@ -0,0 +1,32 @@
+package com.sopt.android3.data.dto.response
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class SeohyunResponseDTO(
+    @SerialName("code")
+    val code: String,
+    @SerialName("message")
+    val message: String,
+    @SerialName("data")
+    val data: SeohyunDataDTO,
+)
+
+@Serializable
+data class SeohyunDataDTO(
+    @SerialName("posts")
+    val posts: List<SeohyunPostDTO>,
+)
+
+@Serializable
+data class SeohyunPostDTO(
+    @SerialName("postId")
+    val postId: Int,
+    @SerialName("title")
+    val title: String,
+    @SerialName("content")
+    val content: String,
+    @SerialName("reactions")
+    val reactions: List<String>,
+)
--- app/src/main/java/com/sopt/android3/presentation/seohyun/SeohyunScreen.kt
+++ app/src/main/java/com/sopt/android3/presentation/seohyun/SeohyunScreen.kt
@@ -2,6 +2,8 @@ package com.sopt.android3.presentation.seohyun
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -15,14 +17,18 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.sopt.android3.R
 import com.sopt.android3.core.designsystem.component.BackTopBar
-import com.sopt.android3.core.designsystem.component.card.Card
+import com.sopt.android3.core.designsystem.component.card.Hashtag
 import com.sopt.android3.core.designsystem.theme.SopkathonTheme
 
 @Composable
@@ -37,28 +43,24 @@ fun SeohyunRoute(
 @Composable
 fun SeohyunScreen(
     modifier: Modifier = Modifier,
+    viewModel: SeohyunViewModel = SeohyunViewModel(),
     navigateToYeseul: () -> Unit = {}
 ) {
-    Column() {
-        BackTopBar(
-            onBackClick = { navigateToYeseul() }
-        )
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    Column {
+        BackTopBar(onBackClick = { navigateToYeseul() })
 
         Spacer(modifier = Modifier.height(40.dp))
 
-        Column(
-            modifier = modifier.padding(horizontal = 30.dp)
-        ) {
+        Column(modifier = modifier.padding(horizontal = 30.dp)) {
             Column(
                 modifier = Modifier
                     .fillMaxSize()
                     .background(color = SopkathonTheme.colors.staticwhite)
             ) {
-                Card(
-                    nickname = "익명A",
-                    content = "본문본문...",
-                    thumbnail = R.drawable.img_card,
-                    hashtags = listOf("슬픔", "그리움"),
+                SeohyunCard(
+                    content = uiState.posts.firstOrNull()?.content ?: "",
                     modifier = Modifier
                         .fillMaxWidth()
                         .height(486.dp)
@@ -66,12 +68,24 @@ fun SeohyunScreen(
 
                 Spacer(modifier = Modifier.height(10.dp))
 
-                Row() {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(
+                            color = SopkathonTheme.colors.gray100,
+                            shape = RoundedCornerShape(50.dp)
+                        )
+                ) {
                     Image(
-                        painterResource(R.drawable.img_card),
+                        painter = painterResource(R.drawable.img_card),
                         contentDescription = null,
-                        modifier = Modifier.clip(shape = RoundedCornerShape(50.dp)).size(30.dp)
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(100.dp))
+                            .size(60.dp)
                     )
+
+                    Spacer(modifier = Modifier.width(10.dp))
+
                     Column {
                         Text(text = "작성자")
                         Spacer(modifier = Modifier.height(10.dp))
@@ -79,10 +93,12 @@ fun SeohyunScreen(
                     }
                 }
 
-
                 Spacer(modifier = Modifier.height(10.dp))
 
-                Row {
+                Row(
+                    modifier = modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
                     Button(onClick = {}) {
                         Text(text = "남겨 놓기")
                     }
@@ -92,6 +108,48 @@ fun SeohyunScreen(
                     }
                 }
             }
+        }
+    }
+}
+
+@Composable
+private fun SeohyunCard(
+    content: String,
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier = modifier
+            .clip(RoundedCornerShape(12.dp))
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.img_card),
+            contentDescription = null,
+            modifier = Modifier.matchParentSize(),
+            contentScale = ContentScale.Crop
+        )
+
+        Column(
+            verticalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .matchParentSize()
+                .background(color = Color.Black.copy(alpha = 0.73f))
+                .padding(vertical = 30.dp, horizontal = 26.dp),
+        ) {
+            Spacer(modifier = Modifier.weight(1f))
+
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(5.dp)
+            ) {
+                Hashtag(label = "3일 전의 고민")
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = content,
+                color = SopkathonTheme.colors.gray100,
+                style = SopkathonTheme.typography.titleM18
+            )
         }
     }
 }
