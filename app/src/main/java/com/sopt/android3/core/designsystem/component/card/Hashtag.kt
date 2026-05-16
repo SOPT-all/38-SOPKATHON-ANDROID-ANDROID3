8ddbb3b9f65670b4b3eeea78ff4f811d367f7ba3
yeseul <joyri0202@gmail.com>
2026-05-17 01:22:55 +0900

    [feat/#7] Hashtag 컴포넌트 구현

---
+++ app/src/main/java/com/sopt/android3/core/designsystem/component/card/Hashtag.kt
@@ -0,0 +1,44 @@
+package com.sopt.android3.core.designsystem.component.card
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sopt.android3.core.designsystem.theme.SopkathonTheme
+
+@Composable
+fun Hashtag(
+    label: String,
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier = modifier
+            .clip(RoundedCornerShape(50))
+            .background(Color.LightGray)
+            .padding(
+                vertical = 1.dp,
+                horizontal = 10.dp
+            ),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "#${label}",
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun HashTagPreview() {
+    SopkathonTheme {
+        Hashtag(label = "이별")
+    }
+}
