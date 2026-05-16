d76a7ef34eae3b2004fd0f14d1c4ba6abafc6142
Hiimynameiss <sh2410152@sookmyung.ac.kr>
2026-05-17 03:03:24 +0900

    feat: #5 화면 UI 전체적으로 구성

--- app/src/main/java/com/sopt/android3/presentation/seohyun/SeohyunScreen.kt
+++ app/src/main/java/com/sopt/android3/presentation/seohyun/SeohyunScreen.kt
@@ -2,31 +2,28 @@ package com.sopt.android3.presentation.seohyun
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.ImagePainter
 import com.sopt.android3.R
+import com.sopt.android3.core.designsystem.component.BackTopBar
 import com.sopt.android3.core.designsystem.component.card.Card
 import com.sopt.android3.core.designsystem.theme.SopkathonTheme
-import com.sopt.android3.core.util.noRippleClickable
 
 @Composable
 fun SeohyunRoute(
@@ -42,44 +39,46 @@ fun SeohyunScreen(
     modifier: Modifier = Modifier,
     navigateToYeseul: () -> Unit = {}
 ) {
-    LazyColumn(
-        modifier = modifier.padding(horizontal = 30.dp)
-    ) {
-        item {
+    Column() {
+        BackTopBar(
+            onBackClick = { navigateToYeseul() }
+        )
+
+        Spacer(modifier = Modifier.height(40.dp))
+
+        Column(
+            modifier = modifier.padding(horizontal = 30.dp)
+        ) {
             Column(
                 modifier = Modifier
                     .fillMaxSize()
                     .background(color = SopkathonTheme.colors.staticwhite)
             ) {
-                Column(
-                    modifier = Modifier.padding(top = 30.dp)
-                ) {
-                    Card(
-                        nickname = "익명A",
-                        content = "본문본문본문본문본문본문본문본문본문본문본문본문본문본문본문본문",
-                        thumbnail = R.drawable.img_card,
-                        hashtags = listOf("슬픔", "그리움"),
-                        modifier = Modifier.height(486.dp)
-                    )
-                }
+                Card(
+                    nickname = "익명A",
+                    content = "본문본문...",
+                    thumbnail = R.drawable.img_card,
+                    hashtags = listOf("슬픔", "그리움"),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(486.dp)
+                )
 
                 Spacer(modifier = Modifier.height(10.dp))
 
-                Column {
-                    Row {
-                        Box(
-                            modifier = Modifier
-                                .clip(shape = RoundedCornerShape(50.dp))
-                                .background(Color.White)
-                        )
-                        Spacer(modifier = Modifier.width(10.dp))
-                        Column {
-                            Text(text = "작성자")
-                            Spacer(modifier = Modifier.height(10.dp))
-                            Text(text = "댓글")
-                        }
+                Row() {
+                    Image(
+                        painterResource(R.drawable.img_card),
+                        contentDescription = null,
+                        modifier = Modifier.clip(shape = RoundedCornerShape(50.dp)).size(30.dp)
+                    )
+                    Column {
+                        Text(text = "작성자")
+                        Spacer(modifier = Modifier.height(10.dp))
+                        Text(text = "댓글")
                     }
                 }
+
 
                 Spacer(modifier = Modifier.height(10.dp))
 
